6d023fcf5a61de60229c5cf24179c05933b8eb11
Raman Aujla <raujla@developrise.com>
2023-03-05 15:31:47 -0500

    reorganized the app component to be clean and moved the component ot the login component, as there can be scenes when loging out would route user to this page

--- src/components/AppComponent/AppComponent.tsx
+++ src/components/AppComponent/AppComponent.tsx
@@ -1,41 +1,12 @@
-import Button from "@mui/material/Button";
-import {
-  Box,
-  TextField,
-  Container,
-  Checkbox,
-  FormControlLabel,
-  Link,
-  Typography,
-  Avatar,
-  CssBaseline,
-  Alert,
-} from "@mui/material";
-import LockOutlinedIcon from "@mui/icons-material/LockOutlined";
-import { BaseSyntheticEvent, Component, ReactNode } from "react";
+import { Component, ReactNode } from "react";
 import { AppComponentStateModel } from "./model/app-component.state.model";
+import { LoginComponent } from "../../scenes/login/LoginComponent";
 
 export class AppComponent extends Component {
   constructor(props: AppComponentStateModel) {
     super(props);
-    this.state = {
-      isValidForm: true,
-      errorMessage: "",
-    };
+    this.state = {};
   }
-
-  handleSubmit = (event: BaseSyntheticEvent): void => {
-    event.preventDefault();
-    const loginData = new FormData(event.currentTarget);
-    const email: string = loginData.get("email") as string;
-
-    if (email.trim().length === 0) {
-      this.setState({
-        isValidForm: false,
-        errorMessage: "Invalid email",
-      });
-    }
-  };
 
   render(): ReactNode {
     const { isValidForm, errorMessage } = this.state as AppComponentStateModel;
@@ -43,82 +14,7 @@ export class AppComponent extends Component {
 
     return (
       <div className="app">
-        <Container component="main" maxWidth="xs">
-          <CssBaseline />
-          <Box
-            sx={{
-              marginTop: "8px",
-              display: "flex",
-              flexDirection: "column",
-              alignItems: "center",
-            }}
-          >
-            <Avatar
-              sx={{
-                // @todo - center component responsively
-                marginTop: "120px",
-                marginBottom: "5px",
-                // @todo - update the color based on the ThemeProvider once setup
-                backgroundColor: "#1976d2",
-              }}
-            >
-              <LockOutlinedIcon />
-            </Avatar>
-
-            <Typography fontSize="20px">Login</Typography>
-
-            <Box
-              component="form"
-              onSubmit={this.handleSubmit}
-              sx={{ marginTop: "1px" }}
-            >
-              <TextField
-                label="Email Address"
-                margin="normal"
-                required
-                fullWidth
-                id="email"
-                name="email"
-                autoComplete="email"
-                autoFocus
-              ></TextField>
-
-              {!isValidForm && <Alert severity="error">{errorMessage}</Alert>}
-
-              <TextField
-                label="Password"
-                margin="normal"
-                required
-                fullWidth
-                id="password"
-                name="password"
-                autoComplete="current-password"
-                type="password"
-              ></TextField>
-
-              <FormControlLabel
-                control={<Checkbox value="remember" color="primary" />}
-                label="Remember Me"
-              />
-
-              <Button
-                type="submit"
-                fullWidth
-                variant="contained"
-                sx={{
-                  marginTop: "3px",
-                  marginBottom: "3px",
-                }}
-              >
-                Login
-              </Button>
-            </Box>
-
-            <Link href="#" variant="body2" alignSelf="flex-end">
-              Forgot Password?
-            </Link>
-          </Box>
-        </Container>
+        <LoginComponent />
       </div>
     );
   }
--- src/scenes/login/LoginComponent.tsx
+++ src/scenes/login/LoginComponent.tsx
@@ -1,7 +1,134 @@
-import { Component, ReactNode } from "react";
+import {
+  Container,
+  CssBaseline,
+  Box,
+  Avatar,
+  Typography,
+  TextField,
+  Alert,
+  FormControlLabel,
+  Checkbox,
+  Button,
+  Link,
+} from "@mui/material";
+import LockOutlinedIcon from "@mui/icons-material/LockOutlined";
+import { BaseSyntheticEvent, Component, ReactNode } from "react";
+import { AppComponentStateModel } from "../../components/AppComponent/model/app-component.state.model";
 
 export class LoginComponent extends Component {
+  constructor(props: AppComponentStateModel) {
+    super(props);
+    this.state = {
+      isValidForm: true,
+      errorMessage: "",
+    };
+  }
+
+  handleSubmit = (event: BaseSyntheticEvent): void => {
+    event.preventDefault();
+    const loginData = new FormData(event.currentTarget);
+    const email: string = loginData.get("email") as string;
+
+    if (email.trim().length === 0) {
+      this.setState({
+        isValidForm: false,
+        errorMessage: "Invalid email",
+      });
+    }
+  };
+
+  handleChange = (event: BaseSyntheticEvent): void => {
+    event.preventDefault();
+    this.setState({
+      isValidForm: true,
+      errorMessage: "",
+    });
+  };
+
   render(): ReactNode {
-    return <div></div>;
+    const { isValidForm, errorMessage } = this.state as AppComponentStateModel;
+    console.log(isValidForm, errorMessage);
+
+    return (
+      <div className="app">
+        <Container component="main" maxWidth="xs">
+          <CssBaseline />
+          <Box
+            sx={{
+              marginTop: "8px",
+              display: "flex",
+              flexDirection: "column",
+              alignItems: "center",
+            }}
+          >
+            <Avatar
+              sx={{
+                // @todo - center component responsively
+                marginTop: "120px",
+                marginBottom: "5px",
+                // @todo - update the color based on the ThemeProvider once setup
+                backgroundColor: "#1976d2",
+              }}
+            >
+              <LockOutlinedIcon />
+            </Avatar>
+
+            <Typography fontSize="20px">Login</Typography>
+
+            <Box
+              component="form"
+              onSubmit={this.handleSubmit}
+              sx={{ marginTop: "1px" }}
+            >
+              <TextField
+                label="Email Address"
+                margin="normal"
+                required
+                fullWidth
+                id="email"
+                name="email"
+                autoComplete="email"
+                autoFocus
+                onChange={this.handleChange}
+              ></TextField>
+
+              {!isValidForm && <Alert severity="error">{errorMessage}</Alert>}
+
+              <TextField
+                label="Password"
+                margin="normal"
+                required
+                fullWidth
+                id="password"
+                name="password"
+                autoComplete="current-password"
+                type="password"
+              ></TextField>
+
+              <FormControlLabel
+                control={<Checkbox value="remember" color="primary" />}
+                label="Remember Me"
+              />
+
+              <Button
+                type="submit"
+                fullWidth
+                variant="contained"
+                sx={{
+                  marginTop: "3px",
+                  marginBottom: "3px",
+                }}
+              >
+                Login
+              </Button>
+            </Box>
+
+            <Link href="#" variant="body2" alignSelf="flex-end">
+              Forgot Password?
+            </Link>
+          </Box>
+        </Container>
+      </div>
+    );
   }
 }
