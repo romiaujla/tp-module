81e3990fa1ea16578ff2e43763834cd26b339091
Raman Aujla <raujla@developrise.com>
2023-03-07 22:42:32 -0500

    wired up the ThemeProvider

--- src/components/App/AppComponent.tsx
+++ src/components/App/AppComponent.tsx
@@ -1,7 +1,9 @@
+import { ThemeProvider } from "@emotion/react";
 import { Component, ReactElement, ReactNode } from "react";
 import { useNavigate } from "react-router-dom";
 import LoginComponent from "../../scenes/Login/LoginComponent";
 import { AppComponentStateModel } from "./model/app-component.state.model";
+import theme from "../../theme";
 
 export default class AppComponent extends Component<AppComponentStateModel> {
   constructor(props: AppComponentStateModel) {
@@ -14,13 +16,15 @@ export default class AppComponent extends Component<AppComponentStateModel> {
   render(): ReactNode {
     const { navigate } = this.state as AppComponentStateModel;
     return (
-      <div className="app">
-        <LoginComponent
-          isValidForm={true}
-          errorMessage=""
-          navigate={navigate}
-        />
-      </div>
+      <ThemeProvider theme={theme}>
+        <div className="app">
+          <LoginComponent
+            isValidForm={true}
+            errorMessage=""
+            navigate={navigate}
+          />
+        </div>
+      </ThemeProvider>
     );
   }
 }
