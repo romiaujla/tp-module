8c12e4c73f9e0707a06310f4daa1ebf1790feae6
Raman Aujla <raujla@developrise.com>
2023-03-08 01:34:20 -0500

    update theme with primary and secondary colors

--- src/theme.ts
+++ src/theme.ts
@@ -1,11 +1,32 @@
 import { createTheme } from "@mui/material";
-import { red } from "@mui/material/colors";
 
 const theme = createTheme({
   palette: {
     primary: {
-      main: red[500],
+      100: "#cce1f1",
+      200: "#99c4e4",
+      300: "#66a6d6",
+      400: "#3389c9",
+      500: "#006bbb",
+      600: "#005696",
+      700: "#004070",
+      800: "#002b4b",
+      900: "#001525",
     },
+    secondary: {
+      100: "#d6ecf9",
+      200: "#acd9f3",
+      300: "#83c6ec",
+      400: "#59b3e6",
+      500: "#30a0e0",
+      600: "#2680b3",
+      700: "#1d6086",
+      800: "#13405a",
+      900: "#0a202d",
+    },
+  },
+  typography: {
+    fontFamily: "Open Sans, sans-serif",
   },
 });
 
