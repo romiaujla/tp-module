397b8a102cbdbdfa5d5a11e08a2fa109916bccb9
Raman Aujla <raujla@developrise.com>
2023-03-09 02:09:31 -0500

    converted the login component to a functional component

--- src/components/App/AppComponent.tsx
+++ src/components/App/AppComponent.tsx
@@ -1,20 +1,13 @@
 import { ThemeProvider } from "@emotion/react";
 import { ReactElement } from "react";
-import { useNavigate } from "react-router-dom";
-import LoginComponent from "../../scenes/Login/LoginComponent";
+import { LoginComponent } from "../../scenes/Login/LoginComponent";
 import theme from "../../theme";
 
 export const AppComponent = (): ReactElement => {
-  const navigate = useNavigate();
-
   return (
     <ThemeProvider theme={theme}>
       <div className="app">
-        <LoginComponent
-          isValidForm={true}
-          errorMessage=""
-          navigate={navigate}
-        />
+        <LoginComponent isValidForm={true} errorMessage="" />
       </div>
     </ThemeProvider>
   );
