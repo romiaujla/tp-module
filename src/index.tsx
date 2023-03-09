fb564bb751a30837d08e5de27551125e11e898d4
Raman Aujla <raujla@developrise.com>
2023-03-09 02:17:33 -0500

    code cleanup, getting rid of path strings

--- src/index.tsx
+++ src/index.tsx
@@ -2,12 +2,13 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import { AppComponent } from "./components/App/AppComponent";
+import { RouterPathEnum } from "./global/enum/router-path.enum";
 import "./index.css";
 import { DashboardComponent } from "./scenes/Dashboard/DashboardComponent";
 
 const router = createBrowserRouter([
-  { path: "/", element: <AppComponent /> },
-  { path: "/dashboard", element: <DashboardComponent /> },
+  { path: RouterPathEnum.APP_PATH, element: <AppComponent /> },
+  { path: RouterPathEnum.DASHBOARD_PATH, element: <DashboardComponent /> },
 ]);
 
 const root = ReactDOM.createRoot(document.getElementById("root")!);
